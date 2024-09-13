661ccdd82fa5505335dfaaebdccf9ea1abd5de05
Riley Cleavenger <73538882+rileycleavenger@users.noreply.github.com>
2024-09-13 13:35:14 -0400

    Time: 1328 ms (59.24%) | Memory: 55.3 MB (64.02%) - LeetSync

---
+++ 204-count-primes/count-primes.py
@@ -0,0 +1,19 @@
+class Solution:
+    def countPrimes(self, n: int) -> int:
+        
+        if n < 2:
+            return 0
+        
+        composites = [False]*n
+        composites[0], composites[1] = True, True
+        
+        for i in range(2, int(sqrt(n)+1)):
+            if not composites[i]:
+                for j in range(i*i, n, i):
+                    composites[j] = True
+        
+        count = 0
+        for x in composites:
+            if not x: count+=1
+        
+        return count
