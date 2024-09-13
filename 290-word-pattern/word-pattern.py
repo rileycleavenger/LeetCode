d3aa92f365a4b634615001c0651a701d4ee5e92e
Riley Cleavenger <73538882+rileycleavenger@users.noreply.github.com>
2024-09-13 12:54:15 -0400

    Time: 25 ms (96.86%) | Memory: 16.4 MB (97.13%) - LeetSync

--- 290-word-pattern/word-pattern.py
+++ 290-word-pattern/word-pattern.py
@@ -3,13 +3,12 @@ class Solution:
         
         words = s.split(' ')
         
-        if len(words) != len(pattern):
-            return False
+        if len(words) != len(pattern): return False
         
         wordToChar = {}
         charToWord = {}
         
-        for w, c, in zip(words, pattern):
+        for w, c in zip(words, pattern):
             if w in wordToChar and wordToChar[w] != c:
                 return False
             if c in charToWord and charToWord[c] != w:
