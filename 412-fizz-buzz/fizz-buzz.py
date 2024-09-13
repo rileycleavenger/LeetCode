4005a682a3887e2d7317fabe8276a5817abc794d
Riley Cleavenger <73538882+rileycleavenger@users.noreply.github.com>
2024-09-13 14:28:34 -0400

    Time: 45 ms (40.88%) | Memory: 17.7 MB (45.95%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.py
+++ 412-fizz-buzz/fizz-buzz.py
@@ -1,17 +1,17 @@
 class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
         
-        res = [""]*(n+1)
+        res = []
         
         for i in range(1, n+1):
             
             if i % 15 == 0:
-                res[i] = "FizzBuzz"
+                res.append("FizzBuzz")
             elif i % 5 == 0:
-                res[i] = "Buzz"
+                res.append("Buzz")
             elif i % 3 == 0:
-                res[i] = "Fizz"
+                res.append("Fizz")
             else:
-                res[i] = str(i)
+                res.append(str(i))
         
-        return res[1:]
+        return res
