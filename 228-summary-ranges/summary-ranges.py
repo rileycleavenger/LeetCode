4723eba5fe63133eaa91a567561ee6102ff12d1d
Riley Cleavenger <73538882+rileycleavenger@users.noreply.github.com>
2024-09-18 13:57:31 -0400

    Time: 31 ms (78.28%) | Memory: 16.5 MB (68.91%) - LeetSync

--- 228-summary-ranges/summary-ranges.py
+++ 228-summary-ranges/summary-ranges.py
@@ -7,12 +7,12 @@ class Solution:
         res = []
         start = 0
         
-        for end in range(1, len(nums)+1):
+        for end in range(1, 1 + len(nums)):
             if end == len(nums) or nums[end] != nums[end-1]+1:
                 if start == end-1:
                     res.append(str(nums[start]))
                 else:
                     res.append(str(nums[start])+"->"+str(nums[end-1]))
                 start = end
-        
+                
         return res
