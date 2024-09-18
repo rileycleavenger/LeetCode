3d2629fa0fc9b9316cf511d57c57e0bd7d2e84cf
Riley Cleavenger <73538882+rileycleavenger@users.noreply.github.com>
2024-09-18 14:34:19 -0400

    Time: 45 ms (78.55%) | Memory: 19.5 MB (9.24%) - LeetSync

--- 203-remove-linked-list-elements/remove-linked-list-elements.py
+++ 203-remove-linked-list-elements/remove-linked-list-elements.py
@@ -9,14 +9,13 @@ class Solution:
         dummy = ListNode()
         dummy.next = head
         prev = dummy
-        
         curr = head
+        
         while curr:
             if curr.val == val:
                 prev.next = curr.next
             else:
                 prev = curr
             curr = curr.next
-            
+        
         return dummy.next
-            
