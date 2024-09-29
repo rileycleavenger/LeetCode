e69c55920a7e9ff5f149c739ddb37e680cf19094
Riley Cleavenger <73538882+rileycleavenger@users.noreply.github.com>
2024-09-29 12:11:43 -0400

    Time: 408 ms (89.15%) | Memory: 34.6 MB (16.35%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -1,11 +1,10 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        
         hash = {}
         
-        for x in nums:
-            hash[x] = hash.get(x, 0) + 1
-            if x in hash and hash[x] > 1:
-                return True 
+        for num in nums:
+            if num in hash:
+                return True
+            hash[num] = 1
         
         return False
