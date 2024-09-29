e0d3ec364d921a81359669f03a176c78ceb45a4b
Riley Cleavenger <73538882+rileycleavenger@users.noreply.github.com>
2024-09-29 12:03:00 -0400

    Time: 1330 ms (57.15%) | Memory: 55.3 MB (65.72%) - LeetSync

--- 204-count-primes/count-primes.py
+++ 204-count-primes/count-primes.py
@@ -7,13 +7,13 @@ class Solution:
         composites = [False]*n
         composites[0], composites[1] = True, True
         
-        for i in range(2, int(1+sqrt(n))):
+        for i in range(2, int(sqrt(n)+1)):
             if not composites[i]:
                 for j in range(i*i, n, i):
                     composites[j] = True
         
         count = 0
         for x in composites:
-            if not x: count +=1
-        
+            if not x: count += 1
+                
         return count
