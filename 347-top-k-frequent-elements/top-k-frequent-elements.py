3582642ecced1338f3c73dc3b7224667a1590203
Riley Cleavenger <73538882+rileycleavenger@users.noreply.github.com>
2024-09-30 13:59:11 -0400

    Time: 85 ms (76.35%) | Memory: 21.2 MB (69.54%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,20 +1,17 @@
-from typing import List
-
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        # Step 1: Count the frequency of each number
-        hash_map = {}
+        
+        hash = {}
         
         for x in nums:
-            hash_map[x] = hash_map.get(x, 0) + 1
+            hash[x] = hash.get(x, 0) + 1
+            
+        sorted_hash = sorted(hash.items(), key=lambda item: item[1], reverse=True)
         
-        # Step 2: Sort the items based on frequency
-        # `sorted` returns a list of tuples (key, value)
-        sorted_items = sorted(hash_map.items(), key=lambda item: item[1], reverse=True)
-        
-        # Step 3: Collect the top k frequent elements
         res = []
-        for i in range(k):
-            res.append(sorted_items[i][0])  # Append the key (the number)
-
-        return res
+        for x in sorted_hash:
+            res.append(x[0])
+            if len(res) == k:
+                return res
+        
+        return []
